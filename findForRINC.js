a6cb22aa967770ca6e02ebeb9760f611a4fff9a7
DakCreed <dashnak96@gmail.com>
2024-04-22 17:17:23 +0300

    Парсинг таблицы restab с помощью cheerio

--- findForRINC.js
+++ findForRINC.js
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-import {flattenObjectArrays, parsePage} from './parseCheerio.js';
+// import {flattenObjectArrays, parsePage} from './parseCheerio.js';
+import readline from 'readline';
 
 const baseURLAuthorSearch='https://elibrary.ru/authors.asp';
 const baseURLAuthorsTo=`https://elibrary.ru/author_items.asp?authorid=110&show_refs=1&show_option=1`
@@ -46,14 +47,16 @@ async function someFunc(inputdata) {
     const clearSelector = '#show_param > table:nth-child(6) > tbody > tr:nth-child(2) > td:nth-child(5) > div';
     await page.click(clearSelector);
 
-    //Выбираем поиск по библиотеке e-lib
+    //Выбираем поиск в библиотеке по показателю e-lib
     const setElibrarySelect = '#show_param > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(3) > div > select';
     await page.select(setElibrarySelect, '2');
 
     const regexSPIN=/^\d{4}-\d{4}$/;
     const regexID=/^\d{1,9}$/;
+
+
     if(regexSPIN.test(inputdata)){
-        console.log('Это спин код');
+        console.log('\nЭто спин код');
         //меняем тип параметра для поиска
         const selectSelector = '#codetype';//находим тип параметра для поиска на странице
         await page.select(selectSelector, optionValue.SPIN);//выбираем параметр для поиска
@@ -63,7 +66,7 @@ async function someFunc(inputdata) {
         await page.type(inputSelector, inputdata);
 
     }else if(regexID.test(inputdata)){
-        console.log('Это id автора')
+        console.log('\nЭто id автора')
         //меняем тип параметра для поиска
         const selectSelector = '#codetype';//находим тип параметра для поиска на странице
         await page.select(selectSelector, optionValue.RSCI);//выбираем параметр для поиска
@@ -77,7 +80,7 @@ async function someFunc(inputdata) {
         console.log('Скорей всего вы ввели имя')
         // Ввести фамилию
         const inputSelector = 'input#surname'; // Замените на нужный селектор
-        await page.type(inputSelector, 'Мосева');
+        await page.type(inputSelector, inputdata);
     }
 
 
@@ -103,16 +106,88 @@ async function someFunc(inputdata) {
 
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-    // Находим ссылку на все публикации автора
-    const hrefValue = await page.$eval('#restab > tbody > tr:nth-child(4) > td:nth-child(4) > div > a:nth-child(1)',
-        (link, baseURL) => {
-            return `${baseURL}${link.getAttribute('href')}`; // Получаем значение атрибута href
-        },
-        baseURL // Передайте baseURL в качестве аргумента функции
-    );
+    const foundAuthorsSelector = `body > table > tbody > tr > td > table:nth-child(1) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr > td > div:nth-child(3) > table > tbody > tr > td.redref > font > b:nth-child(1) > font`;
+    const foundAuthorsSelectorTextContent = await page.evaluate((selector) => {
+        const element = document.querySelector(selector);
+        return element ? element.textContent : null;
+    }, foundAuthorsSelector);
+    console.log(`По данному запросу количество найденных авторов : ${foundAuthorsSelectorTextContent} `);
 
-    await page.goto(hrefValue, { waitUntil: 'networkidle0' });
-    console.log(hrefValue);
+
+
+    // Находим ссылку на все публикации автора
+    // const hrefValue = await page.$eval('#restab > tbody > tr:nth-child(4) > td:nth-child(4) > div > a:nth-child(1)',
+    //     (link, baseURL) => {
+    //         return `${baseURL}${link.getAttribute('href')}`; // Получаем значение атрибута href
+    //     },
+    //     baseURL // Передайте baseURL в качестве аргумента функции
+    // );
+    //
+    // await page.goto(hrefValue, { waitUntil: 'networkidle0' });
+    // console.log(hrefValue);
+
+    //Получаем Html-код таблицы restab, в которой хранятся ссылки на авторов
+    // const restabTable = await page.$eval('#restab > tbody', element => element.innerHTML)
 }
-someFunc(inputsParams.IDMosevaAl);
+// someFunc(inputsParams.IDMosevaAl);
 // someFunc(inputsParams.SPINPotapchenko);
+someFunc("Мосева");
+const people = [
+    { name: "Иван Иванов", hightSchool: "МГУ", linksToArticles: ["link1", "link2", "link3", "link4"] },
+    { name: "Петр Петров", hightSchool: "СПбГУ", linksToArticles: ["link1", "link2", "link3", "link4"] },
+    { name: "Алина Кузнецова", hightSchool: "НГУ", linksToArticles: ["link5", "link6", "link7"] },
+    { name: "Олег Савельев", hightSchool: "УрФУ", linksToArticles: ["link8", "link9"] },
+    { name: "Мария Чернова", hightSchool: "ТГУ", linksToArticles: ["link10", "link11", "link12", "link13"] },
+    { name: "Сергей Дмитриев", hightSchool: "ВШЭ", linksToArticles: ["link14", "link15", "link16", "link17", "link18"] }
+];
+
+async function askUserToArrayObject(arr) {
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            prompt: 'Выберите номер объекта и нажмите Enter: '
+        });
+
+        // Выводим первые два свойства каждого объекта.
+        console.log('Доступные объекты:');
+        arr.forEach((item, index) => {
+            console.log(`${index + 1}: ${item.name}, ${item.hightSchool}`);
+        });
+
+        rl.prompt();
+
+        // Обработчик ввода.
+        rl.on('line', (line) => {
+            const index = parseInt(line.trim(), 10) - 1;
+
+            if (index >= 0 && index < arr.length) {
+                // Если номер валиден, выводим весь объект.
+                console.log('Выбран объект:');
+                console.log(arr[index]);
+                rl.close();
+                resolve(arr[index]);
+            } else {
+                // Если введен неверный номер, сообщаем об ошибке и перезапрашиваем ввод.
+                console.log('Некорректный ввод. Пожалуйста, выберите правильный номер.');
+                rl.prompt();
+            }
+        });
+
+        rl.on('close', () => {
+            resolve(null);
+        });
+    });
+}
+// (async () => {
+//     try {
+//         const selectedObject = await askUserToArrayObject(people);
+//         if (selectedObject) {
+//             console.log('Вы успешно выбрали объект:', selectedObject);
+//         } else {
+//             console.log('Выход из программы или объект не был выбран.');
+//         }
+//     } catch (error) {
+//         console.error('Произошла ошибка:', error);
+//     }
+// })();
--- parseCheerio.js
+++ parseCheerio.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import * as cheerio from 'cheerio';
 
+
 export function parseCheerioElement(findArr){
 
     let finalSetArticlesPage=[];
@@ -79,4 +80,128 @@ export function parsePage(loadedHTML){
 
 export function flattenObjectArrays(arrayOfArrays) {
     return arrayOfArrays.flat();
+}
+
+try {
+    // Синхронное чтение файла
+    const data = fs.readFileSync('restab Мосева.txt', 'utf8');
+    // Вывод содержимого файла
+    // console.log('Данные файла:', data);
+    parseRestabTable(data);
+} catch (err) {
+    // Обработка ошибок (например, если файл не найден)
+    console.error('Ошибка при чтении файла:', err);
+}
+
+
+function parseRestabTable(restab){
+    let arrAuthorsObj=[]
+
+    //Загрузили таблицу #restab в объект cheerio
+    let $restab = cheerio.load(restab, null, false);
+    const persons = $restab('tbody').children('tr[valign="top"][bgcolor="#f5f5f5"]');
+
+    for (let elemPersons of persons) {
+        let authorsObj = {
+            nameAuthor: null,
+            listOfPublications: null,
+            titleHightSchool: null,
+            NumberOfPublications: null
+        };
+
+        try {
+            const id = $restab(elemPersons).attr('id');
+            if (!id) {
+                throw new Error("ID не найден для элемента person");
+            }
+            authorsObj.listOfPublications = `https://elibrary.ru/author_items.asp?authorid=${deleteA(id)}&show_refs=1&show_option=1`;
+
+            const nameAuthor = $restab(elemPersons).find('td.midtext').find('font[color="#00008f"]').find('b:last').text();
+            if (!nameAuthor) {
+                throw new Error("Не удалось найти имя автора в предоставленном HTML");
+            }
+            authorsObj.nameAuthor = nameAuthor;
+
+            let $element = $restab(elemPersons);
+
+            let brElement=$element.find('br:last');
+            // Сначала находим родительский элемент, так как `.contents()` возвращает все дочерние узлы, включая текстовые
+            let parentOfBr = brElement.parent();
+
+// Получаем все узлы после 'br:last' включая текстовые узлы
+            let allContents = parentOfBr.contents();
+            let textAfterBr = "";
+
+// Находим индекс последнего 'br' в дочерних узлах
+            let brIndex = allContents.index(brElement) + 1;  // +1 чтобы начать поиск с элемента следующего за br
+
+// Итерируемся по всем узлам после последнего <br>
+            allContents.slice(brIndex).each(function() {
+                if (this.nodeType === 3) {  // nodeType 3 соответствует текстовому узлу
+                    textAfterBr += $element(this).text();
+                }
+            });
+
+            console.log(textAfterBr.trim());
+
+            console.log(brElement.length)
+
+            console.log($element.html())
+            console.log(authorsObj)
+
+
+        } catch (error) {
+            console.error("Ошибка при обработке данных элемента:", error.message);
+            continue; // Пропускаем текущий элемент и продолжаем со следующим
+        }
+
+        arrAuthorsObj.push(authorsObj);
+    }
+
+    // persons.each((index, element) => {
+    //     const id = $restab(element).attr('id');
+    //     authorsObj.listOfPublications=`https://elibrary.ru/author_items.asp?authorid=${deleteA(id)}&show_refs=1&show_option=1`;
+    //     const htmlContent = $restab(element).html();
+    //     const nameAuthor = $restab(element).find('td.midtext').find('font[color="#00008f"]').find('b:last').text();
+    //     authorsObj.nameAuthor=nameAuthor;
+    //     let brElements=$restab(element).find('br');
+    //     brElements.each((index, brElement) => {
+    //         // Получить следующий элемент после <br>
+    //         const nextNode = brElement.nextSibling;
+    //         // Проверить, является ли следующий элемент текстовым узлом
+    //         if (nextNode && nextNode.nodeType === 3) {
+    //             // Вывести текст следующего узла
+    //             authorsObj.titleHightSchool=nextNode.nodeValue.trim();
+    //         }
+    //     });
+    //     arrAuthorsObj.push(authorsObj);
+    // });
+
+    // let linkContents = [];
+    // persons.each((index, person) => {
+    //     // Найти все теги <a> с атрибутом title
+    //     const links = $restab(person).find('a[title="Список публикаций данного автора на портале elibrary.ru"]');
+    //
+    //     // Пройтись по каждому найденному тегу <a> и добавить его содержимое в массив
+    //     links.each((i, link) => {
+    //         const content = $restab(link).text(); // Получить текстовое содержимое тега <a>
+    //         linkContents.push(content); // Добавить содержимое в массив
+    //     });
+    // });
+
+    // console.log(linkContents);
+    console.log(arrAuthorsObj)
+
+
+}
+
+function deleteA(str){
+    return str.replace(/^a/, '');
+}
+
+function removeLeadingNumbersAndDot(str) {
+    // Регулярное выражение для поиска цифр, точки и пробела в начале строки
+    const regex = /^[\d.]+\s*/;
+    // Заменяем найденное регулярным выражением пустую строку
+    return str.replace(regex, '');
 }
